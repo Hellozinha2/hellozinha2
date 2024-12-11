3e884faac57e9131c9cc739116d51593485d080e
Hellozinha2 <maneraheloisa@gmail.com>
2024-12-11 11:43:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Quais são os três tipos de Biologia?',
+    'Fisiologia, genética e Histologia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'A capital da Argentina é Buenos Aires'
+)
+
+criaCartao(
+    'Animal',
+    'Qual é o maior mamífero do mundo?',
+    'O maior mamífero do mundo é a Baleia Azul'
+)
+
+criaCartao(
+    'Perigoso',
+    'Qual o animal mais perigoso do mundo?',
+    'O animal mais perigoso é a vespa-do-mar'
+)
